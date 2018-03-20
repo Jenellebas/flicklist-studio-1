6f95056b8c1b008d3cddd902b83355389ebe0e6f
Jenelle Basinger <jenellebas@yahoo.com>
2018-03-19 19:06:03 -0500

    finished Studio1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import random
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
@@ -17,13 +17,23 @@ def index():
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
-
+    tomorrow_movie = get_random_movie()
+    content += "<h1>Tomorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + tomorrow_movie + "</li>"
+    content += "</ul>"
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    movie_list = ["Best in Show", "Mystic Pizza", "Truman Show", "The Natural", "Hello, Dolly"]
+    
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
-
+    x=0
+    for title in movie_list:
+        x += 1
+    
+    dice=random.randrange(0,x)
+    return(movie_list[dice])
 
 app.run()
